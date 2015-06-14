78ab6d78a1a51f4f83f5f14c459203950fd383de
Aaron Yap <aynycda@gmail.com>
2015-06-14 09:58:20 -0400

    added #create action to PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,4 +19,15 @@ class PostsController < ApplicationController
 		return redirect_to(posts_path)
 	end
 
+	def create
+
+		if Post.create(title:params[:title],post:params[:post])
+			flash[:notice] = "Last Action:  Post Created (Title: #{params[:title]})"
+		else
+			flash[:alert] = "Last Action: Post could not be created."
+		end
+
+		return redirect_to(posts_path)
+	end
+
 end
